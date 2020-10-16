f3674b0d84a5bbb14489bedb5bbf87cc8276854e
lileiguan <904520880@qq.com>
2020-10-16 17:26:20 +0800

    1

--- src/todo/manage.vue
+++ src/todo/manage.vue
@@ -9,7 +9,8 @@
 <!--						<el-menu-item index="addJob"><img src="../assets/smallimg/guanzhi.png"></img>官职表(不分页)</el-menu-item>
 -->						<el-menu-item index="/commonDCT?dctid=guanzhi"><img src="../assets/smallimg/guanzhi.png"></img>官职表</el-menu-item>
 						<el-menu-item index="/commonDCT?dctid=dept"><img src="../assets/smallimg/user.png"></img>部门表</el-menu-item>
-						<el-menu-item index="/commonDCT?dctid=county"><img src="../assets/smallimg/county.png"></img>州府表</el-menu-item>
+						<el-menu-item index="/commonDCT?dctid=population"><img src="../assets/smallimg/county.png"></img>人口表</el-menu-item>
+						<el-menu-item index="/commonDCT?dctid=county"><img src="../assets/smallimg/county.png"></img>州县表</el-menu-item>
 						<el-menu-item index="/commonDCT?dctid=person"><img src="../assets/smallimg/renwu.png"></img>人物表</el-menu-item>
 						<el-menu-item index="/commonDCT?dctid=jobTime"><img src="../assets/smallimg/guanzhi.png"></img>履历表</el-menu-item>
 						<el-menu-item index="/commonDCT?dctid=attribute"><img src="../assets/smallimg/attribute.png"></img>特性表</el-menu-item>
